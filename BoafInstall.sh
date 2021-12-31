0b1b010b6f3a682e4a5642a168f4ecbcdae7fb05
Logang2 <49832778+Logang2@users.noreply.github.com>
2021-12-31 15:44:31 +0000

    Update BoafInstall.sh

--- BoafInstall.sh
+++ BoafInstall.sh
@@ -2,11 +2,15 @@
 
 cd /home/pi/
 mkdir Dexter
+cd Dexter
 git clone https://github.com/DexterInd/GrovePi
 cd /home/pi/Dexter/GrovePi/Script
 bash ./update_grovepi.sh
 echo "After installation turn off pi and add grove pi attachment"
 
+cd
+cd Desktop
+cd Boaf
 cd Main
 cd GPS
 gcc -o GPS main.c
