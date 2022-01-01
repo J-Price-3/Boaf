f6ad7938e0d17b87ec9f243efd3214c80141786e
Logang2 <49832778+Logang2@users.noreply.github.com>
2022-01-01 18:22:56 +0000

    Update BoafPosition.py

--- Main/BoafPosition.py
+++ Main/BoafPosition.py
@@ -4,10 +4,17 @@ def GetLatitude():
     file = open("GPSDATA.txt", "r")
     ret = file.readlines()
     file.close()
-    return ret
+    return ret[0]
 
+def GetLongitude():
+    file = open("GPSDATA.txt", "r")
+    ret = file.readlines()
+    file.close()
+    return ret[1]
 
+latitude = GetLatitude()
+longitude = GetLongitude()
 
 while True:
     print(GetLatitude())
-    time.sleep(5)
+    time.sleep(0.5)
