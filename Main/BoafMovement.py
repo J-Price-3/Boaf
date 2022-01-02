608c2b162ed64855f35cbaabe8b75f4e4e10ba28
Logang2 <49832778+Logang2@users.noreply.github.com>
2022-01-02 13:03:17 +0000

    Update BoafMovement.py

--- Main/BoafMovement.py
+++ Main/BoafMovement.py
@@ -2,17 +2,30 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(37, GPIO.OUT)
-rudder = GPIO.PWM(37, 50)
-rudderDuty = 2
-rudder.start(0)
-time.sleep(2)
-while rudderDuty <= 12:
-    rudder.ChangeDutyCycle(rudderDuty)
-    time.sleep(1)
-    rudderDuty += 1
 
-time.sleep(1)
+class Rudder:
+    def __init__(self):
+        self.rudder = GPIO.PWM(37, 50)
+        self.duty = 7
+        self.rudder.start()
+        self.rudder.ChangeDutyCycle(self.duty)
+    
+    def setAngle(self, angle):
+        self.duty = angle/18 + 7
+    
+    def update(self):
+        self.rudder.ChangeDutyCycle(self.duty)
 
-rudder.ChangeDutyCycle(2)
+rudder = Rudder()
+
+rudder.update()
+time.sleep(5)
+rudder.setAngle(90)
+rudder.update()
+time.sleep(5)
+rudder.setAngle(-69)
+time.sleep(5)
+
+rudder.rudder.stop()
+GPIO.cleanup()
