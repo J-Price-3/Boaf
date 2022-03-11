3be440594cadd892185dc49ae0f65aa336970148
Logang2 <49832778+Logang2@users.noreply.github.com>
2022-03-11 17:27:52 +0000

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -1,7 +1,9 @@
+from turtle import pos, position
 import BoafSensors, BoafPosition, BoafMovement, BoafPathfinding
 import csv
 from datetime import datetime
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -50,14 +52,63 @@ def Update(increment):
     pathfinder.Update()
 
 
+def Run():
+    #### Do Measurements ####
 
-#### Do Measurements ####
+    propeller.setSpeed(100)
 
-propeller.setSpeed(100)
+    while not pathfinder.Done():
+        Update(0.1)
 
-while not pathfinder.Done():
-    Update(0.1)
+    ####/Do Measurements/####
+    propeller.stop()
 
-####/Do Measurements/####
+def SystemCheck():
+    print("motor running")
+    propeller.setSpeed(100)
+    time.sleep(1)
+    propeller.setSpeed(0)
+    print("motor stopped")
+    input("enter to continue")
+    rudder.setAngle(0)
+    print("rudder to 0")
+    time.sleep(3)
+    print("rudder to 90")
+    rudder.setAngle(90)
+    time.sleep(3)
+    print("rudder to -90")
+    rudder.setAngle(-90)
+    time.sleep(3)
+    input("enter to continue")
+    print("testing positon reader")
+    t = 0
+    while t < 10:
+        positionReader.Update(0.1)
+        time.sleep(0.1)
+        t += 0.1
+        if(t % 1 == 0):
+            print("pos:" + positionReader.GetPosition())
+            print("vel:" + positionReader.GetVelocity())
+    input("enter to continue")
+    print("testing sensors")
+    t = 0
+    while t < 10:
+        phReader.Update(0.1)
+        tdsReader.Update(0.1)
+        turbidityReader.Update(0.1)
+        time.sleep(0.1)
+        t += 0.1
+        if(t % 1 == 0):
+            print("ph:" + phReader.GetMovingAverage())
+            print("tds:" + tdsReader.GetMovingAverage()())
+            print("turb:" + turbidityReader.GetMovingAverage()())
+    input("enter to continue")
+    print("testing laser")
+    while t < 10:
+        obstacleDetector.Update(0.1)
+        time.sleep(0.1)
+        t += 0.1
+        if(t % 1 == 0):
+            print("obstacle:" + obstacleDetector.valid)
+    input("enter to continue")    
 
-propeller.stop()
